0a934d6d1a6cf10c76a84c4f4515c8fb21b97dd9
jloman <jloman200@gmail.com>
2023-03-01 10:51:17 +0300

    Added test for the show methods

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,11 +8,26 @@ RSpec.describe 'Users', type: :request do
     end
 
     it('rendered :index template') do
-      get '/users/index'
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+    it 'body should includes correct placeholder text' do
+      get '/users'
+      expect(response.body).to include('Here is a list of users for a given user')
+    end
+  end
+
+  describe 'GET /show' do
+    it 'status should be :ok' do
+      get '/users/show'
+      expect(response).to have_http_status(:ok)
+    end
+    it 'should render :show template' do
+      get '/users/show'
       expect(response).to render_template(:show)
     end
     it 'body should includes correct placeholder text' do
-      get '/users/index'
+      get '/users/show'
       expect(response.body).to include('Here is a single user')
     end
   end
