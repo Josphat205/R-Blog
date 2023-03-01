06dd5839026f392c77a0678319331ca62b332d04
jloman <jloman200@gmail.com>
2023-03-01 10:51:39 +0300

    Add test for the show method in posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Posts', type: :request do
     before(:each) do
       @user = User.create(name: 'James', posts_counter: 0, photo: '', bio: '')
       @post = Post.create(title: 'Title', text: 'Text', author_id: @user.id)
-      get "/users/#{@user.id}/posts/index"
+      get "/users/#{@user.id}/posts"
     end
 
     it('status should be :ok') do
@@ -12,9 +12,27 @@ RSpec.describe 'Posts', type: :request do
     end
 
     it('should render :show template') do
-      expect(response).to render_template(:show)
+      expect(response).to render_template(:index)
     end
 
+    it 'body should includes correct placeholder text' do
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
+  end
+  
+
+  describe 'GET /show' do
+    before(:each) do
+      @user = User.create(name: 'James', posts_counter: 0, photo: '', bio: '')
+      @post = Post.create(title: 'Title', text: 'Text', author_id: @user.id)
+      get "/users/#{@user.id}/posts/show"
+    end
+    it 'status should be :ok' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'should render :show template' do
+      expect(response).to render_template(:show)
+    end
     it 'body should includes correct placeholder text' do
       expect(response.body).to include('Here is a single post')
     end
