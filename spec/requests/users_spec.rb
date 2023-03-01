293a425026d465ee4c59a6f4659e6908059c6fdf
jloman <jloman200@gmail.com>
2023-03-01 09:35:56 +0300

    linters configuration

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,22 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    
-    
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
     it('status should be :ok') do
-      get "/users/index"
+      get '/users/index'
       expect(response).to have_http_status(:ok)
     end
 
     it('rendered :index template') do
-      get "/users/index"
+      get '/users/index'
       expect(response).to render_template(:show)
     end
     it 'body should includes correct placeholder text' do
-      get "/users/index"
+      get '/users/index'
       expect(response.body).to include('Here is a single user')
     end
-    
   end
 end
