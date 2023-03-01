57880a614d7ee1940e2dd53d832054065c3356ac
jloman <jloman200@gmail.com>
2023-03-01 09:34:50 +0300

    Add rpsec to test users controller methods

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Users", type: :request do
+  describe "GET /index" do
+    
+    
+    it('status should be :ok') do
+      get "/users/index"
+      expect(response).to have_http_status(:ok)
+    end
+
+    it('rendered :index template') do
+      get "/users/index"
+      expect(response).to render_template(:show)
+    end
+    it 'body should includes correct placeholder text' do
+      get "/users/index"
+      expect(response.body).to include('Here is a single user')
+    end
+    
   end
 end
