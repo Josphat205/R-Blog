81127018c4af2b0db1bdf87b0f7fcb9df22c278a
jloman <jloman200@gmail.com>
2023-03-03 17:23:15 +0300

    linters config

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,26 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
-    def index; end
-  
-    def new
-      newcomment = Comment.new
-      respond_to do |format|
-        format.html { render :new, locals: { newcomment: } }
-      end
-    end
-  
-    def create
-      @post = Post.find(params[:post_id])
-      @comment = current_user.comments.new(post_id: @post.id, author_id: current_user.id, text: comment_params)
-      @comment.post_id = @post.id
-      @comment.author_id = current_user.id
-        @comment.save
-        flash[:success] = 'Comment saved successfully'
-        redirect_to user_post_path(current_user, @post.id)
-    
-    end
-  
-    def comment_params
-      params.require(:comment).permit(:text)[:text]
+  def index; end
+
+  def new
+    newcomment = Comment.new
+    respond_to do |format|
+      format.html { render :new, locals: { newcomment: } }
     end
   end
-  
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(post_id: @post.id, author_id: current_user.id, text: comment_params)
+    @comment.post_id = @post.id
+    @comment.author_id = current_user.id
+    @comment.save
+    flash[:success] = 'Comment saved successfully'
+    redirect_to user_post_path(current_user, @post.id)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)[:text]
+  end
+end
--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.autoloaders.each do |autoloader|
   autoloader.inflector = Zeitwerk::Inflector.new
   autoloader.inflector.inflect(
