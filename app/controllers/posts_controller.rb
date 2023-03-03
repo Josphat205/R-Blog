fe8a562e1dc0bae713e7adcb637160e757792750
jloman <jloman200@gmail.com>
2023-03-03 17:16:09 +0300

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,34 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts.all
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
-  def create
-    @post = Post.create(post_params)
-    @post.user = current_user
+  def new
+    newpost = Post.new
     respond_to do |format|
-      format.html do
-        if @post.save
-          flash[:success] = 'New Post created successfully'
-          redirect_to user_posts_path(current_user)
-        else
-          flash.now[:error] = 'An error occurred : Post could not be created'
-        end
-      end
+      format.html { render :new, locals: { newpost: } }
     end
   end
 
-  private
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+
+    if @post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to user_post_path(current_user, @post.id)
+    else
+      newpost = Post.new
+      flash.now[:error] = 'Error: post could not be saved'
+      render :new, locals: { newpost: }
+    end
+  end
 
   def post_params
     params.require(:post).permit(:title, :text)
