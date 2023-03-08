babb23ce3129576c2514aec3489013e880778143
Krishna <libraryismyparadise@gmail.com>
2023-03-08 16:25:24 +0545

    Fix N+1 Problem in Post Views

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,6 +16,6 @@ class Post < ApplicationRecord
   end
 
   def fetch_recent_comments
-    comments.order('created_at DESC').last(5)
+  Comment.includes(:post).where(post_id: id).order(created_at: :desc).limit(5)
   end
 end
