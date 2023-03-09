7f77051e2e96ab324046cf3a56490476e63653b7
jloman <jloman200@gmail.com>
2023-03-09 18:43:12 +0300

    Update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -9,6 +7,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio email password password_confirmation])
   end
+
+  private
 
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
