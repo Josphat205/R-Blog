eb0a15ea4d1e096178d02f7381b61a621f1c4d7f
jloman <jloman200@gmail.com>
2023-03-09 18:41:39 +0300

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
   def index
-    @users = User.all.order('created_at DESC')
+    @users = User.all
   end
+
+  def new; end
 
   def show
     @user = User.includes(posts: %i[author]).find(params[:id].to_i)
