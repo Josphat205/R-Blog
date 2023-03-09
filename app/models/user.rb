3ea440ca51c099c7ff09ded508e4eed040d012b5
jloman <jloman200@gmail.com>
2023-03-09 18:42:15 +0300

    Update users model after installing devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable, :confirmable
+  validates :name, :photo, :bio, presence: true
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
-  before_validation :default_value
+  before_validation :default_values
+
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def fetch_recent_posts
