d1fdfc411b25b943752d3a53cc53119972c65cc4
jloman <jloman200@gmail.com>
2023-03-09 16:47:03 +0300

    check linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio email password password_confirmation])
   end
 
-  private
-
-  def after_sign_out_path_for(_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
 end
