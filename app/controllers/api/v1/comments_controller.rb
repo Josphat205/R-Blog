9e30154a4fb78bb5bbbbe458adf04c806af459c8
jloman <jloman200@gmail.com>
2023-03-10 18:05:11 +0300

    linters config

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -24,7 +24,7 @@ class Api::V1::CommentsController < ApplicationController
     render json: { message: 'Comment not found' }, status: :not_found
   end
 
-def create
+  def create
     @comment = Comment.new(comment_params)
     @comment.author = current_user
     @comment.post_id = @post.id
@@ -34,9 +34,10 @@ def create
       render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
   def comment_params
     post = Post.find(params[:post_id])
-    params.require(:comment).permit(:text).merge(post: post)
+    params.require(:comment).permit(:text).merge(post:)
   end
 
   private
