75d879850542dfb12acb3b6d20e982960b233793
jloman <jloman200@gmail.com>
2023-03-10 13:02:11 +0300

    Add Ability class

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    case user.role
+    when 'user'
+      can %i[read create], Post
+      can %i[update destroy], Post, author_id: user.id
+      can %i[read create], Comment
+      can %i[update destroy], Comment, author_id: user.id
+    when 'admin'
+      can :manage, :all
+    else
+      can :read, Post
+    end
+   
+  end
+end
