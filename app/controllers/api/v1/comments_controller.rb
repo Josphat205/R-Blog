f80e97fc464a1d42b4d5c3ab35dda70cb371fc7e
Krishna <libraryismyparadise@gmail.com>
2023-03-10 20:45:15 +0545

    Add create method for comment API

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -24,7 +24,20 @@ class Api::V1::CommentsController < ApplicationController
     render json: { message: 'Comment not found' }, status: :not_found
   end
 
- 
+def create
+    @comment = Comment.new(comment_params)
+    @comment.author = current_user
+    @comment.post_id = @post.id
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  def comment_params
+    post = Post.find(params[:post_id])
+    params.require(:comment).permit(:text).merge(post: post)
+  end
 
   private
 
