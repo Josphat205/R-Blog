73f1f0638426b8b1db1ca16feab9988f29a5d3a4
jloman <jloman200@gmail.com>
2023-03-10 16:41:42 +0300

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,23 @@
-class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+class ApplicationController < ActionController::API
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio email password password_confirmation])
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  def not_found
+    render json: { error: 'not_found' }
   end
 
-  private
-
-  def after_sign_out_path_for(_resource_or_scope)
-    sign_in_path
+  def authorize_request
+    header = request.headers['Authorization']
+    if header
+      header = header.split.last
+      begin
+        @decoded = JsonWebToken.decode(header)
+        @current_user = User.find_by_id!(@decoded[:user_id])
+      rescue ActiveRecord::RecordNotFound || JWT::DecodeError => e
+        render json: { error: e.message }, status: :unauthorized
+      end
+    else
+      render json: { error: 'Unauthorized User' }, status: :unauthorized
+    end
   end
 end
